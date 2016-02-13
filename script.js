1d93ed80f7f5931e475cf3f5f54574ba3b7fd41a
gam0022 <gam0022@gmail.com>
2016-02-13 20:40:35 +0900

    シングルクオートは極力使わない

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ document.body.appendChild(function() {
       var originalHTML = originalBuildMsgHTML(O, h);
       try {
         var target = $(originalHTML);
-        var messageContent = target.children('.message_content');
-        var container = target.children('.action_hover_container');
+        var messageContent = target.children(".message_content");
+        var container = target.children(".action_hover_container");
         var buttonClass = "ts_icon ts_tip ts_tip_top ts_tip_float ts_tip_delay_600 ts_tip_hide ts_tip_hidden";
 
         // Quote Button
-        var url = messageContent.children('a.timestamp').attr("href");
+        var url = messageContent.children("a.timestamp").attr("href");
         var quoteButton = $("<a></a>", {
           "class": "ts_icon_quote " + buttonClass,
           "data-action": "quote",
@@ -20,9 +20,9 @@ document.body.appendChild(function() {
         container.prepend(quoteButton);
 
         // Reply Button
-        var userURL = messageContent.find("a.message_sender").attr('href');
+        var userURL = messageContent.find("a.message_sender").attr("href");
         if (userURL != null) {
-          var user = userURL.split('/')[2];
+          var user = userURL.split("/")[2];
           var message = messageContent.children("span.message_body").text();
           var replyButton = $("<a></a>", {
             "class": "ts_icon_reply " + buttonClass,
@@ -51,20 +51,20 @@ document.body.appendChild(function() {
     };
 
     $(document).on("click", "[data-action='quote']", function(event) {
-      var messageInput = document.getElementById('message-input');
-      var url = $(event.target).data('url');
-      messageInput.value += '\n' + "https://" + location.host + url;
+      var messageInput = document.getElementById("message-input");
+      var url = $(event.target).data("url");
+      messageInput.value += "\n" + "https://" + location.host + url;
       messageInput.focus();
-      $('#message-input').trigger("autosize").trigger("autosize-resize");
+      $("#message-input").trigger("autosize").trigger("autosize-resize");
     });
 
     $(document).on("click", "[data-action='reply']", function(event) {
-      var messageInput = document.getElementById('message-input');
-      var user = $(event.target).data('user');
-      var message = $(event.target).data('message');
-      messageInput.value = "@" + user + ':\n' + ( message ? '>' + message + '\n' : "" ) + messageInput.value;
+      var messageInput = document.getElementById("message-input");
+      var user = $(event.target).data("user");
+      var message = $(event.target).data("message");
+      messageInput.value = "@" + user + ":\n" + ( message ? ">" + message + "\n" : "" ) + messageInput.value;
       messageInput.focus();
-      $('#message-input').trigger("autosize").trigger("autosize-resize");
+      $("#message-input").trigger("autosize").trigger("autosize-resize");
     });
   };
 
