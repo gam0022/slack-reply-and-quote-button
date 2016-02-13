159c976699b170deb21bd1f1bb2c7db38f924379
gam0022 <gam0022@gmail.com>
2016-02-13 19:02:54 +0900

    onclickを排除

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.body.appendChild(function() {
 
       // Quote Button
       var refUrl = msgContent.children('a.timestamp').attr("href");
-      var quoteButton = $('<a data-action="quote" class="' + buttonClass + '" data-refurl="' + refUrl + '" onclick="onQuoteClick(this)"><span class="ts_tip_tip">Quote</span>Qt</a>');
+      var quoteButton = $('<a data-action="quote" class="ts_icon_quote ' + buttonClass + '" data-refurl="' + refUrl + '"><span class="ts_tip_tip">Quote</span></a>');
       quoteButton = container.find('[data-action="copy_link"]').after(quoteButton);
 
       // Reply Button
@@ -21,7 +21,7 @@ document.body.appendChild(function() {
       if (senderHref != null) {
         var senderUser = senderHref.split('/')[2];
         var repMessage = msgContent.children("span.message_body").text();
-        var replyButton = $('<a data-action="reply" class="' + buttonClass + '" data-user="' + senderUser + '" data-repMes="' + repMessage + '" onclick="onReplyClick(this)"><span class="ts_tip_tip">Reply</span>Re</a>');
+        var replyButton = $('<a data-action="reply" class="ts_icon_reply ' + buttonClass + '" data-user="' + senderUser + '" data-repMes="' + repMessage + '"><span class="ts_tip_tip">Reply</span></a>');
         quoteButton.after(replyButton);
       }
 
@@ -36,19 +36,19 @@ document.body.appendChild(function() {
       return html;
     }
 
-    onQuoteClick = function(target) {
-      var messageText = document.getElementById('message-input');
-      messageText.value += '\n' + "https://" + location.host + '/' + $(target).data('refurl');
-      messageText.focus();
+    $(document).on("click", '.ts_icon_quote', function(event) {
+      var messageInput = document.getElementById('message-input');
+      messageInput.value += '\n' + "https://" + location.host + '/' + $(event.target).data('refurl');
+      messageInput.focus();
       $('#message-input').trigger("autosize").trigger("autosize-resize");
-    }
+    });
 
-    onReplyClick = function(target){
-      var messageText = document.getElementById('message-input');
-      messageText.value += '@' + $(target).data('user') + ':\n' + ( $(target).data('repmes') ? ( '>' + $(target).data('repmes') + '\n' ) : "" );
-      messageText.focus();
+    $(document).on("click", '.ts_icon_reply', function(event) {
+      var messageInput = document.getElementById('message-input');
+      messageInput.value += '@' + $(event.target).data('user') + ':\n' + ( $(event.target).data('repmes') ? ( '>' + $(event.target).data('repmes') + '\n' ) : "" );
+      messageInput.focus();
       $('#message-input').trigger("autosize").trigger("autosize-resize");
-    }
+    });
   }
 
   src = "(" + code.toString() + ")()"
